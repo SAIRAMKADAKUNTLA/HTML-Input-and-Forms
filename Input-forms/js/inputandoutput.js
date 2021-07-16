ca870b4bf379839f3e7f6bcd75d151ceecf83511
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-07-16 17:13:06 +0530

    [Sairam]Added:Ability to validate the email in the input using the regex

--- Input-forms/js/inputandoutput.js
+++ Input-forms/js/inputandoutput.js
@@ -7,8 +7,16 @@ salary.addEventListener('input',function(){
 const text=document.querySelector('#text');
 const textError=document.querySelector('.text-error');
 text.addEventListener('input',function(){
-    let nameregex=RegExp("^[A-Z]{1}[A-Za-z]{2,}")
+    let nameregex=RegExp("^[A-Z]{1}[A-Za-z]{2,}$")
     if(nameregex.test(text.value))
     textError.textContent="";
     else textError.textContent="Invalid Name";
+});
+const email=document.querySelector('#email');
+const emailError=document.querySelector('.email-error');
+email.addEventListener('input',function(){
+let emailregex=RegExp("[A-Za-z0-9]{3,}[.]{1}[A-Za-z0-9]{3,}[@]{1}[A-Za-z]{4,}[.]{1}[a-z]{3,}")
+if(emailregex.test(email.value))
+emailError.textContent="";
+else emailError.textContent="Invalid Email";
 })
